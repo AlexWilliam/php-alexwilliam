0b7653a057ab147060d855656c42674fff9012c0
AlexWilliam <alexgoncalves.ti@gmail.com>
2022-07-08 22:59:32 -0300

    Arquivos

--- src/Controller/UsuariosController.php
+++ src/Controller/UsuariosController.php
@@ -2,13 +2,26 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Services\UsuariosService;
 
 class UsuariosController {
 
-    public function getUsuarios(): Response {
+    private $usuariosService;
 
-        $response = new Response(json_encode(["nome"=>"Teste"]), 200, ["Content-Type"=>"application/json"]);
+    /*public function __construct(UsuariosService $usuariosService) {
+        $this->usuariosService = $usuariosService;
+    }*/
+
+    public function getUsuarios(Request $params): Response {
+
+        $nome = $params->query->get("nome") ?? "";
+        $email = $params->query->get("email") ?? "";
+
+        $response = new Response(json_encode(["nome"=>$nome, "email"=>$email]), 200, ["Content-Type"=>"application/json"]);
 
         return $response;
     }
